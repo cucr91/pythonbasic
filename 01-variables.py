41f80ad03a0d933c66ad7262153fd971a9413cf9
cucr91 <camilocalderonrojas@gmail.com>
2023-08-15 19:21:29 -0400

    Modificaciones en archivos 01 y 02

--- 01-variables.py
+++ 01-variables.py
@@ -1,5 +1,11 @@
-variable = "Hola Mundo"
+variable = "Hola Mundo" 
 numero = 42
 decimal = 10.5
 verdadero = True
 falso = False
+
+variable = "string"
+numero = 32
+float = 19.5
+bolean = True
+bolean2 = falso
--- 02-strings.py
+++ 02-strings.py
@@ -1,8 +1,9 @@
 texto = "Hola Mundo"
+#metodo  funcion incluida dentro de un objeto 
 print(texto.upper())
 print(texto.lower())
 print(texto.find("Mun"))
 print(texto.find("mun"))
-nuevoTexto = texto.replace("Mun", "chanchito feliz")
+nuevoTexto = texto.replace("Mundo", "chanchito feliz")
 print(texto, nuevoTexto)
 print("Mundo" in texto)
