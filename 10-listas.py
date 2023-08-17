fc0eee688c2bb2d0612c4f2522d0a037b8eba816
cucr91 <camilocalderonrojas@gmail.com>
2023-08-16 20:44:28 -0400

    Se agrega archivos 10-listas y 11-lidas-metodos

---
+++ 10-listas.py
@@ -0,0 +1,6 @@
+lenguajes = ["Python", "Rubi", "Php", "Javascript", "java"]
+lenguajes[1] = "Go"
+print(lenguajes[-3])
+print(lenguajes[1:3])
+print(lenguajes[:3])
+print(lenguajes[1:])
---
+++ 11-listas-metodos.py
@@ -0,0 +1,9 @@
+lenguajes = ["Python", "Rubi", "Php", "Javascript", "java"]
+lenguajes.insert(3, "Go")
+lenguajes.insert(0, "C")
+lenguajes.remove("Rubi")
+print("Php" in  lenguajes)
+print("rubi" in  lenguajes)
+
+print(len(lenguajes))
+
